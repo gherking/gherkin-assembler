41d4e0da8eb4004473279fa055fe7836036f4340
László Szikszai <sziklaszlo@gmail.com>
2017-04-15 22:53:17 +0200

    Added DocString and its test

--- test/ast/Step.spec.js
+++ test/ast/Step.spec.js
@@ -1,9 +1,21 @@
 'use strict';
 
 const path = require('path');
-const Step = require(path.resolve('./lib/ast/Step.js'));
+const Step = require(path.resolve('lib/ast/Step.js'));
+const DocString = require(path.resolve('lib/ast/DocString.js'));
 
 const expect = require('chai').expect;
+
+const stepAst = {
+    type: 'Step', 
+    keyword: 'When ', 
+    text: 'this is a when step',
+    argument: {
+        type: 'DocString',
+        content: 'Hello\nWorld'
+    }
+};
+        
 
 describe('Ast.Step', () => {
     it('should represent an Ast Step instance', () => {
@@ -19,14 +31,16 @@ describe('Ast.Step', () => {
     });
     
     it('should parse Gherkin Ast Step type to Step', () => {
-        const step = Step.parse({type: 'Step', keyword: 'When ', text: 'this is a when step'});
+        const step = Step.parse(stepAst);
         expect(step).to.be.instanceOf(Step);
         expect(step.keyword).to.equal('When');
         expect(step.text).to.equal('this is a when step');
+        expect(step.argument).to.be.instanceOf(DocString);
     });
 
     it('should have proper string representation', () => {
         const step = new Step('When ', 'this is a when step');
-        expect(step.toString()).to.equal('When this is a when step');
+        step.argument = new DocString('Hello\nWorld');
+        expect(step.toString()).to.equal('When this is a when step\n  """\n  Hello\n  World\n  """');
     });
 });
